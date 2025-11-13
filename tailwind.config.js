eaefd4104f65bbc085e033794990754cf2084eab
sapna subedi <sapnasubedi@Mac.lan>
2025-11-13 16:10:48 -0500

    add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        "pink-bg": "#ffc2d1",
+        "time-bg": "#ffe5ec",
+      },
+    },
+  },
   plugins: [],
 };
